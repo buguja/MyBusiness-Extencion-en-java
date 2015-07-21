8fd3f1e7b1120fbd2b4a28e2d38f2e951967b7e1
Javier Buron Gutierrez <abrew_javier@hotmail.com>
2015-07-21 13:43:44 -0500

    clase Ejecutar.java agreagada no hay codigo valioso hasta ahora

---
+++ src/Ejecutar.java
@@ -0,0 +1,13 @@
+/**
+ * @author Javier Burón Gutiérrez; Lizeth Vásquez Rojas.
+ *
+ */
+
+public class Ejecutar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
