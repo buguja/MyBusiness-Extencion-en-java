503aca2b094a62520a1d49c510b4655df5833fd5
Lizeth <Liz_02277@hotmail.com>
2015-07-24 17:19:38 -0500

    Listbox implementado

--- src/GUI/Principal.java
+++ src/GUI/Principal.java
@@ -6,16 +6,20 @@
 package GUI;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextArea;
+import javax.swing.ListSelectionModel;
 import javax.swing.table.DefaultTableModel;
 
 public class Principal extends JFrame implements ActionListener {
@@ -34,23 +38,24 @@ public class Principal extends JFrame implements ActionListener {
 		
 		JPanel panelTituloLbl = new JPanel();
 		JPanel panelInfoLbl = new JPanel();
+		JPanel panelLista = new JPanel();
 		
 		//CREACIÓN DEL CONTENIDO DE LOS PANELES
 		tituloLbl = new JLabel("CONTROL DE INVENTARIO");
 		infoLbl = new JLabel("Desarrollado por: " + "\n" + "Javier Burón Gutierrez | Lizeth Vásquez Rojas");
 		area = new JTextArea("Vista Factura");
-		JScrollPane panel = new JScrollPane(area,JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS); 
-		area.setPreferredSize(new Dimension(200,390));
+		/*JScrollPane panel = new JScrollPane(area,JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS); 
+		area.setPreferredSize(new Dimension(200,390));*/
 		
 		//AGREGANDO CONTENIDO A LOS PANELES
 		panelTituloLbl.add(tituloLbl);
 		panelInfoLbl.add(infoLbl);
-		panelDerecho.add(panel);
 		
 		//AGREGANDO PANEL A LOS PANELES CONTENEDORES
 		panelNorte.setLayout(new GridLayout(1,3));
 		panelNorte.add(panelTituloLbl);
 		panelNorte.add(panelInfoLbl);
+		//panelDerecho.add(panelLista);
 		
 		//MOSTRANDO PANEL EN LA VENTANA
 		add(panelNorte,BorderLayout.NORTH);
@@ -76,6 +81,16 @@ public class Principal extends JFrame implements ActionListener {
 		 tabla.setPreferredScrollableViewportSize(tabla.getPreferredSize());
 		 JScrollPane scrollPane = new JScrollPane(tabla);
 		 getContentPane().add(scrollPane,BorderLayout.CENTER); 
+		 
+		 //CREACIÓN DE LA LISTA
+		 String[] nombres = {"CFDI-MAVR791010KM9-A-3338","CFDI-MAVR791010KM9-A-3338","CFDI-MAVR791010KM9-A-3338","Divian", 
+							 "Leslie","Paz","Andrea","Macarena","Margarita","Daniela","Divian", 
+							 "Leslie","Paz","Andrea","Macarena","Margarita","Daniela","Divian", 
+							 "Leslie","Paz","Andrea","CFDI-MAVR791010KM9-A-3338","CFDI-MAVR791010KM9-A-3338","CFDI-MAVR791010KM9-A-3338"
+							 ,"CFDI-MAVR791010KM9-A-3338","CFDI-MAVR791010KM9-A-3338"};
+		 JList lista = new JList(nombres);
+		 JScrollPane scroll = new JScrollPane(lista);
+		 getContentPane().add(scroll, BorderLayout.EAST);
 				 
 		//ESCUCHAR A LOS EVENTOS
 		//botonSalir.addActionListener(this);
@@ -122,8 +137,7 @@ public class Principal extends JFrame implements ActionListener {
 				{"AH018F1", "Factura 2", new Boolean(true)},
 				{"AH018F1", "Factura 2", new Boolean(true)},
 				{"AH018F1", "Factura 2", new Boolean(true)},
-		};
-		
+		}; 
 		new Principal(datos);
 	}
 }
